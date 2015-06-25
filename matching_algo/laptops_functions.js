77c205f498b0aae094f06cd5c82c5418d41f8807
UnlockPrice <admin@unlockprice.com>
2015-06-25 21:15:34 +0530

    Changed on 25/06/15

--- matching_algo/laptops_functions.js
+++ matching_algo/laptops_functions.js
@@ -56,6 +56,8 @@ function function_calculate(arr1,key1,opr,arr2,key2)
 		return keyAndSubsequenceExists(arr1,key1,arr2,key2);
 	else if(opr == "ASE")
 		return keyAndArraySubsetEqualExists(arr1,key1,arr2,key2);
+	else if(opr == "LCSU")
+		return keyAndLCSUExists(arr1,key1,arr2,key2);
 	else if(opr == "AND")
 		return (key1 && key2);
 	else if(opr == "OR")
@@ -88,6 +90,20 @@ function keyAndArraySubsetEqualExists(arr1,key1,arr2,key2)
 	return rulesExceptions(arr1,key1,arr2,key2);
 }
 
+function keyAndLCSUExists(arr1,key1,arr2,key2)
+{
+	if(  arr1.hasOwnProperty(key1) && arr2.hasOwnProperty(key2) && arr1[key1] && arr2[key2] )
+	{
+		var minlen = ( arr1[key1].length < arr2[key2].length ) ? (arr1[key1].length):(arr2[key2].length);
+		if( findLongestCommonSubstring(arr1[key1],arr2[key2]) >= minlen-3 )
+			return true;
+		else
+			return false;
+	}
+	else
+	return rulesExceptions(arr1,key1,arr2,key2);
+}
+
 function substringExists(str1,str2)
 {
 	if( str1.indexOf(str2)!=-1 )
@@ -104,7 +120,7 @@ function keyAndSubstringExists(arr1,key1,arr2,key2)
 }
 function keyAndSubsequenceExists(arr1,key1,arr2,key2)
 {
-	if( ( arr1.hasOwnProperty(key1) && arr2.hasOwnProperty(key2)) && arr1[key1] && arr2[key2] && mySubsequence(arr1[key1],arr2[key2]) )
+	if( ( arr1.hasOwnProperty(key1) && arr2.hasOwnProperty(key2)) && arr1[key1] && arr2[key2] && arr1[key1]!="-1" && arr2[key2]!="-1" && mySubsequence(arr1[key1],arr2[key2]) )
 	return true;
 	else
 	return rulesExceptions(arr1,key1,arr2,key2);
@@ -143,4 +159,60 @@ function rulesExceptions(arr1,key1,arr2,key2)
 		return true;
 	else
 		return false;
+}
+
+function findLongestCommonSubstring(a,b)
+{
+  var longest = "";
+  // loop through the first string
+  for (var i = 0; i < a.length; ++i) {
+    // loop through the second string
+    for (var j = 0; j < b.length; ++j) {
+      // if it's the same letter
+      if (a[i] === b[j]) {
+        var str = a[i];
+        var k = 1;
+        // keep going until the letters no longer match, or we reach end
+        while (i+k < a.length && j+k < b.length // haven't reached end
+               && a[i+k] === b[j+k]) { // same letter
+          str += a[i+k];
+          ++k;
+        }
+        // if this substring is longer than the longest, save it as the longest
+        if (str.length > longest.length) { longest = str }
+      }
+    }
+  }
+  return longest;
+}
+
+var longest_common_substring = function(S, T) {
+	
+	
+	var D = [];
+	var LCS_len = 0;
+	var LCS = [];
+
+	for (var i = 0; i < S.length; i++) {
+		D[i] = [];
+		for (var j = 0; j < T.length; j++) {
+			if (S[i] == T[j]) {
+				if (i == 0 || j == 0) {
+					D[i][j] = 1;
+				} else {
+					D[i][j] = D[i-1][j-1] + 1;
+				}
+
+				if (D[i][j] > LCS_len) {
+					LCS_len = D[i][j];
+					LCS = [S.substring(i-LCS_len+1, i+1)];
+				} else if (D[i][j] == LCS_len) {
+					LCS.push(S.substring(i-LCS_len+1, i+1));
+				}
+			} else {
+				D[i][j] = 0;
+			}
+		}
+	}
+	return [D, {"LCS": LCS}, {"LCS_len": LCS_len}];
 }
